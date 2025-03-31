7bbe2f9f1901ef9713ceb0a53674b73c2a81c781
91tleg <maxchou0417@gmail.com>
2025-03-31 12:43:55 -0700

    refactor: remove unused callback functions

--- src/button.c
+++ src/button.c
@@ -1,16 +1,12 @@
 #include "button.h"
+#include "uart.h"
 
-static button_state_t state = OCS_INIT;
+static button_state_e state = OCS_INIT;
 static unsigned long last_debounce_time = 0;
 static unsigned long press_start_time = 0;
 static unsigned int click_count = 0;
 
-void (*click_callback)(void) = NULL;
-void (*double_click_callback)(void) = NULL;
-void (*long_press_callback)(void) = NULL;
-void (*long_press_end_callback)(void) = NULL;
-
-int debounce(int value)
+static int debounce(int value)
 {
     static int last_state = 1;
     static int debounced_state = 1;
@@ -29,114 +25,101 @@ int debounce(int value)
     return debounced_state;
 }
 
-void reset_button_state(void)
+static inline void reset_button_state(void)
 {
     state = OCS_INIT;
     click_count = 0;
     press_start_time = 0;
 }
 
-void button_task(void *pvParameter)
+state_event_e read_state_event(void)
 {
-    while (1)
+    int button_state = gpio_get_level(BTN_PIN);
+    int debounced_state = debounce(button_state);
+    unsigned long current_time = esp_timer_get_time();
+    unsigned long press_duration = current_time - press_start_time;
+    state_event_e event = STATE_EVENT_NONE;
+
+    switch (state)
     {
-        int button_state = gpio_get_level(BTN_PIN);
-        int debounced_state = debounce(button_state);
-        unsigned long wait_time = esp_timer_get_time() - press_start_time;
-
-        switch (state)
+    case OCS_INIT:
+        if (debounced_state == 0) // Button pressed
         {
-        case OCS_INIT:
-            if (debounced_state == 0)
-            {
-                state = OCS_DOWN;
-                press_start_time = esp_timer_get_time();
-            }
-            break;
-
-        case OCS_DOWN:
-            if (debounced_state == 1)
-            {
-                state = OCS_UP;
-                unsigned long press_duration = esp_timer_get_time() - press_start_time;
-
-                if (press_duration >= LONG_PRESS_TIME)
-                {
-                    if (long_press_callback)
-                    {
-                        long_press_callback();
-                    }
-                    state = OCS_PRESSEND;
-                }
-                else
-                {
-                    state = OCS_COUNT;
-                }
-            }
-            break;
-
-        case OCS_UP:
-            if (debounced_state == 0)
-            {
-                state = OCS_DOWN;
-                press_start_time = esp_timer_get_time();
-            }
-            else
-            {
-                if (wait_time >= SHORT_PRESS_TIME)
-                {
-                    if (click_count == 1 && click_callback)
-                    {
-                        click_callback();
-                    }
-                    else if (click_count == 2 && double_click_callback)
-                    {
-                        double_click_callback();
-                    }
-                    reset_button_state();
-                }
-            }
-            break;
-
-        case OCS_COUNT:
-            if (debounced_state == 0)
-            {
-                state = OCS_DOWN;
-                press_start_time = esp_timer_get_time();
-            }
-            else
-            {
-                if (wait_time >= DOUBLE_CLICK_TIME)
-                {
-                    if (click_count == 1 && click_callback)
-                    {
-                        click_callback();
-                    }
-                    else if (click_count == 2 && double_click_callback)
-                    {
-                        double_click_callback();
-                    }
-                    reset_button_state();
-                }
-            }
-            break;
-
-        case OCS_PRESSEND:
-            if (debounced_state == 1)
-            {
-                if (long_press_end_callback)
-                {
-                    long_press_end_callback();
-                }
-                reset_button_state();
-            }
-            break;
-
-        default:
-            reset_button_state();
-            break;
+            state = OCS_DOWN;
+            press_start_time = current_time;
         }
+        break;
 
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+    case OCS_DOWN:
+        if (debounced_state == 1) // Button released
+        {
+            if (press_duration >= LONG_PRESS_TIME)
+            {
+                state = OCS_PRESSEND;
+                event = STATE_EVENT_BUTTON_LONG_PRESS;
+            }
+            else
+            {
+                click_count++;
+                state = OCS_COUNT; // Wait to check for double-click
+            }
+        }
+        break;
+
+    case OCS_UP:
+        if (debounced_state == 0) // Button pressed again
+        {
+            state = OCS_DOWN;
+            press_start_time = current_time;
+        }
+        else if (press_duration >= SHORT_PRESS_TIME)
+        {
+            if (click_count == 1)
+            {
+                event = STATE_EVENT_BUTTON_PRESS;
+            }
+            else if (click_count == 2)
+            {
+                event = STATE_EVENT_BUTTON_DOUBLE_PRESS;
+            }
+            reset_button_state();
+        }
+        break;
+
+    case OCS_COUNT:
+        if (debounced_state == 0)
+        {
+            state = OCS_DOWN;
+            press_start_time = current_time;
+        }
+        else if (press_duration >= DOUBLE_CLICK_TIME)
+        {
+            if (click_count == 1)
+            {
+                event = STATE_EVENT_BUTTON_PRESS;
+            }
+            else if (click_count == 2)
+            {
+                event = STATE_EVENT_BUTTON_DOUBLE_PRESS;
+            }
+            reset_button_state();
+        }
+        break;
+
+    case OCS_PRESS:
+        if (debounced_state == 1) // Button released
+        {
+            state = OCS_PRESSEND;
+        }
+        break;
+
+    case OCS_PRESSEND:
+        if (debounced_state == 1) // Button fully released
+        {
+            reset_button_state();
+        }
+        break;
     }
+
+    return event;
 }
