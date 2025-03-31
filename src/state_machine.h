73a988aa115340249d1fe9207520e42fc1c408aa
91tleg <maxchou0417@gmail.com>
2025-03-31 12:25:34 -0700

    refactor: move state event enum to button

--- src/state_machine.h
+++ src/state_machine.h
@@ -3,14 +3,10 @@
 
 #include <stdint.h>
 #include <string.h>
-#include "parameters.h"
-
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
-
-typedef struct state_machine_data state_machine_data;
 
 typedef enum
 {
+    STATE_ROMID,
     STATE_BATTERY_V,
     STATE_VEHICLE_SPEED,
     STATE_ENGINE_SPEED,
@@ -38,16 +34,6 @@ typedef enum
     STATE_STORED_CODE_THREE
 } state_e;
 
-typedef enum
-{
-    STATE_EVENT_BUTTON_PRESS,
-    STATE_EVENT_BUTTON_LONG_PRESS,
-    STATE_EVENT_BUTTON_DOUBLE_PRESS
-} state_event_e;
-
-void state_machine_init(struct state_machine_data *data);
-void state_enter(struct state_machine_data *data, state_e from, state_event_e next_event, state_e to);
-void process_event(struct state_machine_data *data, state_event_e next_event);
-void state_machine_run(struct state_machine_data *data, state_event_e event);
+void state_machine_run(void);
 
 #endif
