f9c64a8c7bd06150fb5301e804556ee39510a21e
91tleg <maxchou0417@gmail.com>
2025-04-01 10:26:50 -0700

    feat: initialize button GPIO & add button task for freertos

--- src/button.c
+++ src/button.c
@@ -10,17 +10,15 @@ static int debounce(int value)
 {
     static int last_state = 1;
     static int debounced_state = 1;
-
+    unsigned long current_time = esp_timer_get_time();
     if (value != last_state)
     {
-        last_debounce_time = esp_timer_get_time();
+        last_debounce_time = current_time;
     }
-
-    if ((esp_timer_get_time() - last_debounce_time) > DEBOUNCE_TIME)
+    if ((current_time - last_debounce_time) > DEBOUNCE_TIME)
     {
         debounced_state = value;
     }
-
     last_state = value;
     return debounced_state;
 }
@@ -30,6 +28,18 @@ static inline void reset_button_state(void)
     state = OCS_INIT;
     click_count = 0;
     press_start_time = 0;
+}
+
+static void button_init(void)
+{
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << BTN_PIN),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE};
+    
+    gpio_config(&io_conf);
 }
 
 state_event_e read_state_event(void)
@@ -43,7 +53,7 @@ state_event_e read_state_event(void)
     switch (state)
     {
     case OCS_INIT:
-        if (debounced_state == 0) // Button pressed
+        if (debounced_state == 0)  // Pressed
         {
             state = OCS_DOWN;
             press_start_time = current_time;
@@ -51,39 +61,25 @@ state_event_e read_state_event(void)
         break;
 
     case OCS_DOWN:
-        if (debounced_state == 1) // Button released
+        if (debounced_state == 1)
         {
             if (press_duration >= LONG_PRESS_TIME)
             {
                 state = OCS_PRESSEND;
                 event = STATE_EVENT_BUTTON_LONG_PRESS;
+                printf("Long Press Detected\n");
             }
             else
             {
-                click_count++;
-                state = OCS_COUNT; // Wait to check for double-click
+                state = OCS_UP;
+                press_start_time = current_time;
             }
         }
         break;
 
     case OCS_UP:
-        if (debounced_state == 0) // Button pressed again
-        {
-            state = OCS_DOWN;
-            press_start_time = current_time;
-        }
-        else if (press_duration >= SHORT_PRESS_TIME)
-        {
-            if (click_count == 1)
-            {
-                event = STATE_EVENT_BUTTON_PRESS;
-            }
-            else if (click_count == 2)
-            {
-                event = STATE_EVENT_BUTTON_DOUBLE_PRESS;
-            }
-            reset_button_state();
-        }
+        click_count++;
+        state = OCS_COUNT;
         break;
 
     case OCS_COUNT:
@@ -92,34 +88,47 @@ state_event_e read_state_event(void)
             state = OCS_DOWN;
             press_start_time = current_time;
         }
-        else if (press_duration >= DOUBLE_CLICK_TIME)
+        else if ((current_time - press_start_time) >= DOUBLE_CLICK_TIME)
         {
+            // Times up, determine single or double press
             if (click_count == 1)
             {
                 event = STATE_EVENT_BUTTON_PRESS;
+                printf("Single press\n");
             }
             else if (click_count == 2)
             {
                 event = STATE_EVENT_BUTTON_DOUBLE_PRESS;
+                printf("Double press\n");
             }
             reset_button_state();
         }
         break;
 
     case OCS_PRESS:
-        if (debounced_state == 1) // Button released
+        if (debounced_state == 1)
         {
             state = OCS_PRESSEND;
         }
         break;
 
     case OCS_PRESSEND:
-        if (debounced_state == 1) // Button fully released
-        {
-            reset_button_state();
-        }
+        reset_button_state();
         break;
     }
 
     return event;
 }
+
+void button_task(void *pvParameters)
+{
+    button_init();
+    reset_button_state();
+
+    for (;;)
+    {
+        read_state_event();
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
