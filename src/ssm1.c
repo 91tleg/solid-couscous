ad1db9cf0db87084a59befd82134aea53c97ed8c
91tleg <maxchou0417@gmail.com>
2025-04-19 10:04:48 -0700

    feat: add macros for bit shifts

--- src/ssm1.c
+++ src/ssm1.c
@@ -1,6 +1,11 @@
 #include "ssm1.h"
 #include "uart.h"
 
+#define GET_MSB(addr) ((addr) >> 8)
+#define GET_LSB(addr) ((addr) & 0xFF)
+#define MSB(addr) GET_MSB(addr)
+#define LSB(addr) GET_LSB(addr)
+
 static inline void stop_read(void)
 {
     static const uint8_t stop_command[4] = {0x12, 0x00, 0x00, 0x00};
@@ -39,7 +44,7 @@ void get_romid(uint8_t *buffer)
 
 __attribute__((weak)) uint8_t read_data_from_address(uint16_t addr)
 {
-    uint8_t read_command[4] = {0x78, (uint8_t)(addr >> 8), (uint8_t)(addr & 0xFF), 0x00};
+    uint8_t read_command[4] = {0x78, (uint8_t)(MSB(addr)), (uint8_t)(LSB(addr)), 0x00};
     static uint8_t answer[3] = {0};
 
     uart_write_bytes(UART_NUM, (const char *)read_command, sizeof(read_command));
@@ -49,12 +54,11 @@ __attribute__((weak)) uint8_t read_data_from_address(uint16_t addr)
     while (retries < 3)
     {
         // Read 3 bytes from UART
-        int nbytes = uart_read_bytes(UART_NUM, answer, 3, pdMS_TO_TICKS(100));
-        if ((nbytes == 3) && (answer[0] == read_command[1]) && (answer[1] == read_command[2]))
+        int num_bytes = uart_read_bytes(UART_NUM, answer, 3, pdMS_TO_TICKS(100));
+        if ((num_bytes == 3) && (answer[0] == read_command[1]) && (answer[1] == read_command[2]))
         {
             break; // Valid response received
         }
-
         ++retries;
     }
     stop_read();
@@ -63,7 +67,7 @@ __attribute__((weak)) uint8_t read_data_from_address(uint16_t addr)
 
 void send_clear_command(uint16_t addr)
 {
-    uint8_t clear_command[4] = {0xAA, (uint8_t)(addr >> 8), (uint8_t)(addr & 0xff), 0x00};
+    uint8_t clear_command[4] = {0xAA, (uint8_t)(MSB(addr)), (uint8_t)(LSB(addr)), 0x00};
     uart_write_bytes(UART_NUM, (const char *)clear_command, sizeof(clear_command));
     uart_flush(UART_NUM);
 }
