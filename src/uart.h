e38f83eb58da0c62007a5fcd58dd7784ee216cb7
91tleg <maxchou0417@gmail.com>
2025-05-26 10:49:42 -0700

    fix: change baud rate to 1953

--- src/uart.h
+++ src/uart.h
@@ -3,9 +3,15 @@
 #include "driver/uart.h"
 
 #ifdef SSM1
-    #define UART_NUM (UART_NUM_1)
+    #define BAUD_RATE   (1953)
+    #define UART_NUM    (UART_NUM_2)
+    #define TX_PIN      (GPIO_NUM_25)
+    #define RX_PIN      (GPIO_NUM_26)
 #else
-    #define UART_NUM (UART_NUM_0)
+    #define BAUD_RATE   (1953)
+    #define UART_NUM    (UART_NUM_0)
+    #define TX_PIN      (GPIO_NUM_1)
+    #define RX_PIN      (GPIO_NUM_3)
 #endif
 
 void uart_init(void);
