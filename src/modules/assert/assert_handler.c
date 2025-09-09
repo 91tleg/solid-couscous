0a7e5229b476604b16f3371791132a2de30c5ea4
91tleg <maxchou0417@gmail.com>
2025-09-08 17:52:19 -0700

    refactor(assert): use led driver

--- src/modules/assert/assert_handler.c
+++ src/modules/assert/assert_handler.c
@@ -1,36 +1,24 @@
 #include "assert_handler.h"
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
+#include "drivers/led/led.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_log.h>
 
-#define LED_PIN (GPIO_NUM_2)
-
-static void led_pin_init(void)
-{
-    gpio_config_t io_conf = {
-        .pin_bit_mask = 1ULL << LED_PIN,
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE
-    };
-    gpio_config(&io_conf);
-}
+#define TAG  "ASSERT"
 
 static void assert_trace(uint32_t program_counter)
 {
-    ESP_LOGI("ERROR", "ASSERT FAILED AT PC 0x%08" PRIx32, program_counter);
+    ESP_LOGE(TAG, "ASSERT FAILED AT PC 0x%08" PRIx32, program_counter);
 }
 
 static void assert_blink_led(void)
 {
-    led_pin_init();
+    led_init();
     for(;;)
     {
-        gpio_set_level(LED_PIN, 1);
+        led_on();
         vTaskDelay(pdMS_TO_TICKS(300));
-        gpio_set_level(LED_PIN, 0);
+        led_off();
         vTaskDelay(pdMS_TO_TICKS(300));
     }
 }
