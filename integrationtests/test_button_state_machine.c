df434379b69c90f9d5e50a94e10446de56c71d04
91tleg <maxchou0417@gmail.com>
2025-09-24 12:43:25 -0700

    test(integration): add test for button to state machine communication

---
+++ integrationtests/test_button_state_machine.c
@@ -0,0 +1,39 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <freertos/task.h>
+#include "mocks/button/button_task.h"
+#include "mocks/state_machine/state_machine_task.h"
+
+#include "modules/button/button_task.h"
+#include "modules/state_machine/state_machine_task.h"
+#include "state_defs.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+
+void test_button_state_machine(void)
+{
+    button_queue_init();
+    state_machine_queue_init();
+
+    QueueHandle_t button_q = button_get_event_queue();
+    QueueHandle_t lcd_q = state_machine_get_lcd_queue();
+
+    xTaskCreate(button_task, "ButtonTask", 1024, NULL, 1, NULL);
+    xTaskCreate(state_machine_task, "StateTask", 1024, NULL, 1, NULL);
+
+    state_event_e event = STATE_EVENT_BUTTON_PRESS;
+    int res = xQueueSend(button_q, &event, 1000);
+    assert(res == pdPASS);
+
+    vTaskDelay(500);
+
+    struct state_machine_data data;
+    res = xQueueReceive(lcd_q, &data, 1000);
+    assert(res == pdPASS);
+
+    printf("[C TEST] LCD state = %d\n", data.state);
+    assert(data.state == STATE_BATTERY_V);
+
+    printf("[C TEST] Passed: button press propagated to state machine\n");
+}
