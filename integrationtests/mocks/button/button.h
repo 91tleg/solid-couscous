cbd14f04e78a5b7a55f5cfbc37802f43d99e44cd
91tleg <maxchou0417@gmail.com>
2025-10-01 19:31:51 -0700

    fix(mocks): add extern c guards to mock modules

--- integrationtests/mocks/button/button.h
+++ integrationtests/mocks/button/button.h
@@ -3,6 +3,15 @@
 
 #include "state_defs.h"
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 state_event_e read_state_event(void); 
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // MOCK_BUTTON_H
--- integrationtests/mocks/state_machine/state_machine.h
+++ integrationtests/mocks/state_machine/state_machine.h
@@ -3,7 +3,16 @@
 
 #include "state_defs.h"
 
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
 void state_machine_data_init(struct state_machine_data *data);
 void process_event(struct state_machine_data *data, state_event_e event);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // MOCK_STATE_MACHINE_H
