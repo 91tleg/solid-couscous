6d3aa5faba0ee3d8b29212176e2f2f7662dafd84
91tleg <maxchou0417@gmail.com>
2025-11-25 15:14:51 -0800

    refactor(log): simplify log and add NDEBUG flag
    - Disable log when `NDEBUG` is defined.
    - directly wrap esp log macros in header.
    - Don't need source file anymore.

--- src/core/include/log.h
+++ src/core/include/log.h
@@ -1,39 +1,15 @@
 #ifndef LOG_H
 #define LOG_H
 
-typedef enum {
-    LOG_LEVEL_NONE,
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_WARN,
-    LOG_LEVEL_ERROR,
-} log_level_e;
-
-/**
- * @brief Set the log level.
- *
- * @param level Log level.
- */
-void log_level_set(log_level_e level);
-
-/**
- * @brief Log a formatted message.
- *
- * @param level Log level.
- * @param tag   Component tag string.
- * @param fmt   printf-style format string.
- * @param ...   Arguments.
- */
-void log_message(log_level_e level, const char *tag, const char *fmt, ...);
-
-/**
- * @brief Initialize logging.
- * 
- * This function sets the log level based on the board. Enable log when the host UART is free.
- */
-void log_init(void);
-
-#define LOGI(tag, fmt, ...) log_message(LOG_LEVEL_INFO, tag, fmt, ##__VA_ARGS__)
-#define LOGW(tag, fmt, ...) log_message(LOG_LEVEL_WARN, tag, fmt, ##__VA_ARGS__)
-#define LOGE(tag, fmt, ...) log_message(LOG_LEVEL_ERROR, tag, fmt, ##__VA_ARGS__)
+#if !defined(NDEBUG)
+#include <esp_log.h>
+#define LOGI(tag, fmt, ...) ESP_LOGI(tag, fmt, ##__VA_ARGS__)
+#define LOGW(tag, fmt, ...) ESP_LOGW(tag, fmt, ##__VA_ARGS__)
+#define LOGE(tag, fmt, ...) ESP_LOGE(tag, fmt, ##__VA_ARGS__)
+#else
+#define LOGI(tag, fmt, ...) ((void)0)
+#define LOGW(tag, fmt, ...) ((void)0)
+#define LOGE(tag, fmt, ...) ((void)0)
+#endif // NDEBUG
 
 #endif // LOG_H
