9b0a90733243086711bfef9d5f663d828c0ddf7a
91tleg <maxchou0417@gmail.com>
2025-12-15 15:12:25 -0800

    refactor(uart): improve uart read logic
    - Flattened uart read logic.
    - Renamed function from `read_bytes` to `uart_read_from_isr`.
    - Increased isr queue size to 40.

--- src/drivers/include/uart.h
+++ src/drivers/include/uart.h
@@ -6,6 +6,6 @@
 
 void uart_driver_init(void);
 bool send_bytes(const uint8_t *data, uint8_t len);
-int read_bytes(uint8_t *buf, uint32_t max_len, uint32_t timeout);
+int uart_read_from_isr(uint8_t *buf, uint32_t max_len, uint32_t timeout);
 
 #endif // UART_H
